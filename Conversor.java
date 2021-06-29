d8ecfc1a1561979e811999dc1249bf8046721286
silviocdjm <81332335+silviocdjm@users.noreply.github.com>
2021-06-29 00:27:15 -0300

    Criação classe ConversorCor

---
+++ Conversor.java
@@ -0,0 +1,15 @@
+public class Conversor {
+    
+    public Imagem converter(Imagem imagem, ConversorCor conversor){
+        Imagem novo = conversor.getNovoMapa(imagem.getAltura(), imagem.getLargura());
+        for(int iCont = 0; iCont < imagem.getAltura(); iCont++){
+            for(int jCont = 0; jCont < imagem.getLargura(); jCont++){
+                Cor cor = imagem.getPixel(iCont, jCont);
+                Cor novaCor = conversor.converter(cor);
+                novo.setPixel(iCont, jCont, novaCor);
+            }
+        }
+	return novo;
+    }
+    
+}
