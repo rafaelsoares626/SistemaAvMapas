69cdb6469c091c8da40d18118c965762c43517bf
rafaelsoares626 <rafael.soares626@gmail.com>
2021-07-05 19:56:09 -0300

    Ajuste na formula de conversão.

--- Convert_CMYK_To_RGB.java
+++ Convert_CMYK_To_RGB.java
@@ -1,7 +1,7 @@
 public class Convert_CMYK_To_RGB implements ConversorCor {
     
     public Imagem getNovoMapa(int altura, int largura){
-        ImagemRGB novoMapa = new ImagemRGB(altura, largura);
+        ImagemRGB novoMapa = new ImagemRGB(altura, largura, new CorRGB(0, 0, 0));
         return novoMapa;
     }
 
@@ -14,8 +14,7 @@ public class Convert_CMYK_To_RGB implements ConversorCor {
     public CorRGB cmykParaRgb(CorCMYK pixel) {
         int red = (int)(255 * (1 - pixel.getUplaUm()/100) * (1 - pixel.getBlack()/100));
         int green = (int)(255 * (1 - pixel.getUplaDois()/100) * (1- pixel.getBlack()/100));
-        int blue = (int)(255 * (1 - pixel.getUplaTres()/100) * (1 - pixel.getBlack()/100));
-            
+        int blue = (int)(255 * (1 - pixel.getUplaTres()/100) * (1 - pixel.getBlack()/100));           
         CorRGB resultado = new CorRGB(red, green, blue);
         return resultado;
     }
